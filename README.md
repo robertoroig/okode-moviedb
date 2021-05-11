e395f624b1df70973ffa7cdb2893ae13b8595d90
Roberto Roig Pedro <rroigpedro@gmail.com>
2021-05-11 23:17:22 +0200

    Initial project scaffold: Framework and dependencies

---
+++ README.md
@@ -0,0 +1,1 @@
+# okode-moviedb
---
+++ src/main/java/com/robertoroig/okodemoviedb/OkodeMoviedbApplication.java
@@ -0,0 +1,13 @@
+package com.robertoroig.okodemoviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkodeMoviedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkodeMoviedbApplication.class, args);
+	}
+
+}
